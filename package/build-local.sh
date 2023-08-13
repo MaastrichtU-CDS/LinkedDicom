eca097166932b974edb2e48908b067d8ba0d1ef3
Johan van Soest <info@johanvansoest.nl>
2023-08-13 20:17:52 +0200

    Bumped version number

--- package/build-local.sh
+++ package/build-local.sh
@@ -1,4 +1,4 @@
 pip uninstall -y LinkedDicom
 cp ../ontology/LinkedDicom.owl LinkedDicom/LinkedDicom.owl
 python -m build
-pip install ./dist/LinkedDicom-0.2.0-py3-none-any.whl
+pip install ./dist/LinkedDicom-0.3.0-py3-none-any.whl
